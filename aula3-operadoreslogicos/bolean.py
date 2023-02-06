85d6f638c8383ca91991a0914eb77ba3653a1018
24cons427@gmail.com <71324684+LudymillaC@users.noreply.github.com>
2023-02-06 19:10:38 -0300

    Python exercícios

---
+++ aula3-operadoreslogicos/bolean.py
@@ -0,0 +1,14 @@
+
+
+var_verdade = True
+var_falso = False
+
+#Imprimindo o tipo pelo comando "type" que foi atribuido lá no começo do código.
+print(type(var_verdade), type(var_falso))
+
+
+var_verdade = True
+var_falso = False
+
+if var_verdade == True:
+    print('Var_verdade é verdadeiro')
---
+++ aula3-operadoreslogicos/operadores-logicos.py
@@ -0,0 +1,11 @@
+# Comparações: == != > < >= <=
+
+var1 = 12
+var2 = 24
+var3 = 'Verdadeiro'
+var4 = 'Falso'
+
+if var1 == var2:
+    print(var3)
+else: print(var4)
+
